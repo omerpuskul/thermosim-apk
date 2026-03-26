b7c4269185085c873ab87f85ce3c562a38ecd2ab
omerpuskul <142184828+omerpuskul@users.noreply.github.com>
2026-03-27 00:25:20 +0300

    Delete thermosim-apk directory

--- thermosim-apk/web/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import ThermoSim from "./ThermoSim.jsx";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<ThermoSim />);
